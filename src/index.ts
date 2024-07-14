c32cda2a6a60e2c31eef52cc9c12b7a9982ce17a
martinancevski <martinancevski@gmail.com>
2024-07-14 22:00:58 +0200

    Initial commit
    initial commit

---
+++ src/index.ts
@@ -0,0 +1,87 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import formidable from "formidable"
+import fs from "fs"
+import {KitResults} from "./kit.results";
+import {KIT_HPV, KIT_STD} from "./kit.data";
+import XLSX from "xlsx"
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+const excelToJson = require('convert-excel-to-json');
+
+// function run(){
+//   let resultDataXls = "input/elta/data2.xlsx"
+//   let resultJson = excelToJson({
+//     sourceFile:resultDataXls
+//   })
+//   console.log(resultJson)
+//   let kitResult = new KitResults(KIT_STD,resultJson.Result)
+//   let template = kitResult.getResultTemplateJson()
+//   console.log(template)
+//   Utils.writeFile("STD template",template)
+//   let xlsResult = kitResult.getXlsResult()
+//   console.log(xlsResult)
+// }
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`
+    <h2>With <code>"express"</code> npm package</h2>
+    <form action="/api/upload" enctype="multipart/form-data" method="post">
+        <div>
+        <select name="kitType" id="kitType">     
+            <option value="HPV">HPV KIT</option>
+            <option value="STD">STD KIT</option>
+            
+        </select>
+      
+        </div>
+      <div>File: <input type="file" name="testResult" multiple="multiple" /></div>
+      <input type="submit" value="Upload" />
+    </form>
+  `);
+});
+
+app.post('/api/upload', (req, res, next) => {
+  const form = formidable({});
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    console.log(fields)
+    let value = fields["kitType"]!
+    console.log("Value "+value[0])
+    let kits:any = {
+      "HPV":KIT_HPV,
+      "STD":KIT_STD
+    }
+    let file = files["testResult"]![0]
+    let resultJson = excelToJson({
+    sourceFile:file.filepath
+    })
+    let kitResult = new KitResults(kits[value[0]],resultJson.Result)
+    let result = kitResult.getXlsResult()
+
+    let workbook = XLSX.utils.book_new()
+    let tempWorkbook = KitResults.convertJsonToMatrix(result.xlsTable)!
+    let ws = XLSX.utils.aoa_to_sheet(tempWorkbook)
+    XLSX.utils.book_append_sheet(workbook,ws)
+    // let data = XLSX.write(workbook,{})
+    // console.log(data)
+    // res.writeHead(200, {
+    //   'Content-Type': "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet\n",
+    //   'Content-disposition': 'attachment;filename=' + "result.xlsx",
+    //   'Content-Length': data.length
+    // });
+    // res.end(Buffer.from(data, 'binary'));
+    res.json(result);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
---
+++ src/kit.results.ts
@@ -0,0 +1,149 @@
+
+import _ from "lodash"
+export class KitResults{
+    kitSpecification:any
+    resultData:any[]
+
+    constructor(kitSpecification: any, resultData: any[]) {
+        this.kitSpecification = kitSpecification
+        this.resultData = resultData
+        this.resultData.shift() //removes the title row
+    }
+
+    getXlsResult(){
+        // {
+        //     A: 'Well',
+        //         B: 'Sample ID',
+        //     C: 'Sample',
+        //     D: 'Sample Type',
+        //     E: 'Dye',
+        //     F: 'Gene',
+        //     G: 'Tm1(°C)',
+        //     H: 'Peak Value 1',
+        //     I: 'Tm2(°C)',
+        //     J: 'Peak Value 2',
+        //     K: 'Tm3(°C)',
+        //     L: 'Peak Value 3'
+        // },
+
+        let result:any = {}
+
+        for (let row of this.resultData){
+            let sampleId = row.C
+            let dye = row.E
+            let gene = row.F
+            let key = `${dye} ${gene}`
+            let sampleResult:any = {}
+            if (result[sampleId]){
+                sampleResult = result[sampleId]
+            }else{
+                result[sampleId] = sampleResult
+            }
+            sampleResult.id = sampleId
+            sampleResult.IC = ""
+            for (let key in this.kitSpecification){
+                sampleResult[key] = ""
+            }
+        }
+
+        for (let row of this.resultData){
+            let sampleId = row.C
+            let dye = row.E
+            let gene = row.F
+            let key = `${dye} ${gene}`
+            let temperatures = [parseFloat(""+row.G),parseFloat(""+row.I),parseFloat(""+row.K),parseFloat(""+row.M)]
+
+            let sampleResult = result[sampleId]
+            let specifications = this.kitSpecification[key]
+            for (let specKey in specifications){
+                let specData = specifications[specKey]
+                let tempMin = specData.value - specData.delta
+                let tempMax = specData.value + specData.delta
+                let tempFound = false
+                for (let temp of temperatures){
+
+                    if (temp >= tempMin && temp <= tempMax){
+                        tempFound = true
+                    }
+                }
+                if (tempFound){
+
+                    if (specKey === "IC"){
+                        sampleResult.IC += "+"
+                    }
+                    sampleResult[key] += "+"
+
+                }else{
+
+                    if (specKey === "IC"){
+                        sampleResult.IC += "-"
+                    }
+                    sampleResult[key] += "-"
+
+                }
+            }
+
+        }
+        let table:any[] = []
+        for (let key in result){
+            let value = result[key]
+            table.push(value)
+        }
+        // Utils.writeExcelFile("Test file 2 result",table)
+        return {
+            data:result,
+            xlsTable:table
+        }
+    }
+
+    getResultTemplateJson(){
+        let result:any = {}
+        for (let row of this.resultData){
+            // let sampleId = row.C
+            let dye = row.E
+            let gene = row.F
+            let key = `${dye} ${gene}`
+            let temp:any = {}
+            if (result[key]){
+                temp = result[key]
+            }else{
+                result[key] = temp
+            }
+            let parts = gene.split("-")
+            for (let part of parts){
+                temp[part] = {
+                    value:100,
+                    delta:1.5
+                }
+            }
+        }
+        return result
+    }
+    static convertJsonToMatrix(json:any[]){
+        let result:string[][] = []
+        let columns:string[] = []
+        for (let jsonRow of json){
+            for (let column in jsonRow){
+                columns.push(column)
+            }
+        }
+        columns = _.uniq(columns)
+        result.push(columns)
+        for (let jsonRow of json){
+            let tempRow:string[] = []
+            for (let column of columns){
+                let value =  jsonRow[column]
+                // if (!value){
+                //     value = ""
+                // }
+                if (!_.isString(value)){
+                    value = JSON.stringify(value)
+                }
+                tempRow.push(value)
+            }
+            result.push(tempRow)
+        }
+
+        return result
+    }
+}
